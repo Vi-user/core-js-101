2eb44b6da8d4df51ce0728a607049760ee89bd7d
Vi-user <vi.volkova.2712@gmail.com>
2022-11-27 00:00:09 +0300

    feat: add solutions to Date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,9 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  const date = new Date(value);
+  return date.valueOf();
 }
 
 /**
@@ -34,8 +35,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value).valueOf();
 }
 
 
@@ -53,8 +54,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return ((year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0)));
 }
 
 
@@ -73,8 +75,16 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const ms = endDate - startDate;
+  const sssInHour = 60 * 60 * 1000;
+  const sssInMin = 60 * 1000;
+  const sssInSec = 1000;
+  const hours = Math.floor(ms / sssInHour);
+  const minutes = Math.floor((ms - (hours * sssInHour)) / sssInMin);
+  const seconds = Math.floor((ms - (hours * sssInHour) - (minutes * sssInMin)) / sssInSec);
+  const miliSeconds = ms - (hours * sssInHour) - (minutes * sssInMin) - (seconds * sssInSec);
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${miliSeconds.toString().padStart(3, '0')}`;
 }
 
 
@@ -95,6 +105,25 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(/* date */) {
+  // let hours = date.getUTCHours();
+  // hours = (hours === 12) ? 0 : hours;
+  // const minutes = date.getUTCMinutes();
+  //
+  // const minutesDegrees = minutes * 6;
+  // const hoursDegrees = (hours * 30) + (minutesDegrees / 12);
+  //
+  // if (minutesDegrees === hoursDegrees) return 0;
+  // if (Math.abs(minutesDegrees - hoursDegrees) === 180) return Math.PI;
+  //
+  // const maxDegree = Math.max(minutesDegrees, hoursDegrees);
+  // const minDegree = Math.min(minutesDegrees, hoursDegrees);
+  //
+  // if ((maxDegree - minDegree) < 180) {
+  //   const result = ((maxDegree - minDegree) * Math.PI) / 180;
+  //   return Math.abs(result);
+  // }
+  // const result = ((360 - maxDegree + minDegree) * Math.PI) / 180;
+  // return Math.abs(result);
   throw new Error('Not implemented');
 }
 
